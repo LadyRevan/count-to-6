55fe39ceb2b8afdf0c1214ebff744cd51a217624
LadyRevan <lindsey.williams.3200@mail.linnbenton.edu>
2016-11-14 11:33:04 -0800

    Add files via upload

--- tagged-template-strings.js
+++ tagged-template-strings.js
@@ -1,14 +1,5 @@
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
-/*
-    strings[0] = '<b>'
-    strings[1] = ' says</b>: "'
-    strings[2] = '"'
-
-    expressions[0] = argv[2] <-- needs escaping?
-    expressions[1] = argv[3] <-- needs escaping?
-*/
-
 function html(strings, ...expressions) {
     let result = strings[0];
 
