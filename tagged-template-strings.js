c506478f03d444a25c8f4e62dc9aa0e51676825b
LadyRevan <lindsey.williams.3200@mail.linnbenton.edu>
2016-11-14 11:24:05 -0800

    Add files via upload

---
+++ tagged-template-strings.js
@@ -0,0 +1,23 @@
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+/*
+    strings[0] = '<b>'
+    strings[1] = ' says</b>: "'
+    strings[2] = '"'
+
+    expressions[0] = argv[2] <-- needs escaping?
+    expressions[1] = argv[3] <-- needs escaping?
+*/
+
+function html(strings, ...expressions) {
+    let result = strings[0];
+
+    expressions.forEach((expression, index, array) => {
+        result += expression.replace(/&/g, '&amp;')
+                            .replace(/'/g, '&apos;')
+                            .replace(/"/g, '&quot;')
+                            .replace(/</g, '&lt;')
+                            .replace(/>/g, '&gt;') + strings[index + 1];
+    });
+    return result;
+}
